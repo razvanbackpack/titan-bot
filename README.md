2dcca31fd5cc640398f38026fba7dae1a277138a
Răzvan <razvan.truta@outlook.com>
2022-05-17 14:34:49 +0300

    readme and extremely basic setup script so we are coool

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # titan-bot
+Titan is a simple Discord bot developed to learn how Discord bots are made.
 
-list
+# usage
+Download files (either by cloning or making your own fork).
+
+First, run ``` npm run setup ``` to have the script create ```config.json``` for you, or rename ```config-template.json``` into ```config.json``` and fill it with your bot data.
+
+
+Then, run  ```npm start``` and enjoy.
+
+# development
+As of now there are no plans to turn Titan into a fully-fledged bot, but you never know.
---
+++ setup.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+import { Chalk } from 'chalk';
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+
+  
+let chalk = new Chalk();
+let configContents = '{ \
+    "clientId": "",\
+    "guildId": "",\
+    "token": ""\
+}';
+
+console.log(chalk.bgBlueBright('SETUP RUNNING'));
+await fs.writeFile("./config.json", configContents, function(err) {
+    if(err) {
+        return console.log(
+            chalk.redBright("ERROR!") + 'Could not create file ' + chalk.greenBright("config.json") + '. Please create it manually, using the contents from ' + chalk.greenBright("config-template.json")
+        );
+        
+    }
+    console.log(' > ' + chalk.greenBright("config.json") + ' successfully created.');
+}); 
+
+
+delay(1000).then(() => {
+    console.log(chalk.bgBlueBright('SETUP DONE.'));
+    console.log('--- Put your bot data in config.json.');
+    console.log('--- After that, you can run ' + chalk.greenBright('npm start') + ' to start Titan');
+});
